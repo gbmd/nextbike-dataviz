72a002a453fffea3d97bbba562e5951f68724560
Georg Balke <georg.balke@tum.de>
2025-09-29 22:36:37 +0200

    updates

---
+++ assets/desc-C_c5QZRz.js
@@ -0,0 +1,10 @@
+const e=`<!DOCTYPE html>
+<html lang="de">
+<head>
+  <meta charset="UTF-8">
+  <title>European Bike Sharing Systems</title>
+</head>
+<body>  
+</body>
+</html>
+`;export{e as default};
